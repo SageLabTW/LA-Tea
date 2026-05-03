c8d0ead6085c7699d3d656fdbf8717291cfff4ef
Jephian Lin <jephianlin@gmail.com>
2026-05-03 19:23:10 +0800

    add _serve.sh

---
+++ _serve.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+bundle exec jekyll serve --livereload
+
+### rbenv setup
+# rbenv install <version>
+# rbenv local <version>
+# rbenv versions
+
+### rbenv check
+# rbenv which gem
+# which gem
+
+### bundle
+# bundle install
+# bundel update github-pages
